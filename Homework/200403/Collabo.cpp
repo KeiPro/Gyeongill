e224b79518d4ef2bcab770db04fe33ae56082a14
KeiPro <keiprogramo@gmail.com>
2020-04-04 00:04:36 +0900

    upload

--- Homework/200403/Collabo.cpp
+++ Homework/200403/Collabo.cpp
@@ -81,8 +81,6 @@ int main()
 	
 	// 4. 도망가시겠습니까?의 값을 받을 변수
 	
-#pragma region 초기부분 [ 영웅이름 설정~전투는 계속됩니다 ] 출력까지
-
 	cout << "영웅의 이름을 입력하세요 : ";
 	cin >> heroesName;
 
@@ -90,10 +88,6 @@ int main()
 	cin >> difficulty;
 
 	heroesHp = (int)(11 - difficulty * 0.6f); //난이도에 의해 체력부분 설정.
-
-#pragma region 난이도 및 몬스터 개수, 이름, 체력, 좌표 설정.
-
-	//새로 추가된 내용. 난이도 설정하기 및 몬스터 생성
 
 	////////////////////////////////////////////////////////////////////////////////////////////////
 	//  난이도에 따라 몬스터의 개수를 조정해야해서	Monster 구조체 변수를if문 안에서 써야되다보니,      // 
@@ -113,6 +107,7 @@ int main()
 
 #pragma endregion
 	
+#pragma region 난이도 (1~3)
 	if (difficulty < 4) //난이도가 4보다 작을 경우 ( 난이도 1 )
 	{
 		monsterCount = difficultyOne;
@@ -125,50 +120,10 @@ int main()
 			monsters[i].name = gyeongillStuName[rand() % stuNumberTotal];  //몬스터 이름 설정. 중복이름 가능
 			monsters[i].hp = (rand() % 5 + 1); // 1~5중 랜덤으로 체력 부여.
 			
-#pragma region 복붙의 범위
-
-			//몬스터의 좌표를 설정하는 while문.
-			while ( true ) //상점 위치인 경우
-			{
-				positionOverlapCheck = false; //기본을 false로 설정.
-				monsters[i].monX = rand() % mapHeight;
-				monsters[i].monY = rand() % mapWidth;
-
-				if ((monsters[i].monX == myXPosition && monsters[i].monY == myYPosition) //몬스터가 내 위치인 경우
-					|| (monsters[i].monX == storeXPosition && monsters[i].monY == storeYPosition)) //몬스터가 상점 위치인 경우
-				{
-					continue; //다시 while문으로 돌아가 몬스터의 좌표를 다시 설정한다.
-				}
-				
-				//몬스터들끼리 좌표가 겹치지 않기 위해 설정. 
-				//i==0인 경우는 첫 번째 몬스터를 설정하는 것이므로 다른 몬스터의 위치에 영향을 받지 않는다.
-				if(i != 0)
-				{
-					for (int j = 0; j < i; j++)
-					{
-						//전에 설정된 몬스터의 위치와 겹칠 경우 if문 안으로 들어간다.
-						if (monsters[i].monX == monsters[j].monX && monsters[i].monY == monsters[j].monY)
-						{
-							positionOverlapCheck = true;
-							break;
-						}
-
-					}
-
-					//이 if문을 통해 겹치냐는 변수가 true가 되면, continue를 통해 다시 while문을 반복하게 된다.
-					if (positionOverlapCheck == true)
-						continue;
-				}
-
-				break; //몬스터가 내 위치도 아니고, 상점 위치도 아니고, 몬스터들끼리 좌표도 겹치지 않으므로 break를 통해 나간다.
-			}
-
-			cout << i << "번 monsters : " << monsters[i].monX << ", " << monsters[i].monY << endl;
-
-#pragma endregion
-
 		}
 	}
+#pragma endregion
+#pragma region 난이도 (3~6)
 	else if (difficulty < 7) //난이도가 4이상 7미만인 경우 ( 난이도 2 )
 	{
 		monsterCount = difficultyTwo;
@@ -184,6 +139,8 @@ int main()
 			//tmp[i] = rand() % stuNumberTotal;
 		}
 	}
+#pragma endregion
+#pragma region 난이도 (7~8)
 	else if (difficulty < 9)
 	{
 		monsterCount = difficultyThree;
@@ -200,6 +157,8 @@ int main()
 			//tmp[i] = rand() % stuNumberTotal;
 		}
 	}
+#pragma endregion
+#pragma region 난이도 (9~10)
 	else //난이도 9, 10
 	{
 		monsterCount = difficultyFour;
@@ -249,54 +208,115 @@ int main()
 				break; //몬스터가 내 위치도 아니고, 상점 위치도 아니고, 몬스터들끼리 좌표도 겹치지 않으므로 break를 통해 나간다.
 			}
 
-			cout << i << "번 monsters : " << monsters[i].monX << ", " << monsters[i].monY << endl;
-
-#pragma endregion
-
-
-#pragma region
+			cout << i << "번 monsters좌표 : " << monsters[i].monX << ", " << monsters[i].monY << endl;
 
 #pragma endregion
 
 		}
+#pragma region 단순 텍스트 출력
+
+		cout << "< 난이도에 의해 HP와 몬스터를 조정합니다. >" << endl;
+		Sleep(1500);
+
+		cout << "영웅의 HP : " << heroesHp << endl;
+
+		Sleep(1000);
+		cout << "몬스터 숫자 : " << monsterCount << endl;
+
+		Sleep(1500);
+		cout << "모든 몬스터를 다 잡을 때까지 전투는 계속됩니다..." << endl;
+		cout << endl;
+		Sleep(2000);
+
+#pragma endregion
+
+#pragma region 미로 및 히어로, 상점, 몬스터 위치 저장
+
+		//맵에 내 위치, 탈출구, map을 저장하는 for문.
+		for (int i = 0; i < mapHeight; i++)
+		{
+			for (int j = 0; j < mapWidth; j++)
+			{
+				map[i][j] = '#';
+			}
+		}
+
+		map[0][0] = 'O'; //내 위치 설정
+		map[storeXPosition][storeYPosition] = '$'; //상점표시
+		for (int i = 0; i < monsterCount; i++) //몬스터 위치 저장
+		{
+			map[monsters[i].monX][monsters[i].monY] = 'M';
+		}
+		
+#pragma endregion
+
+#pragma region 미로 출력
+		while (true)
+		{
+			system("cls");
+
+			//맵을 계속해서 출력하는 for문
+			for (int i = 0; i < mapHeight; i++)
+			{
+				for (int j = 0; j < mapWidth; j++)
+				{
+					cout << map[i][j];
+				}
+				cout << endl;
+			}
+
+			inputKey = _getch(); //사용자로부터 입력받는 _getch()함수.
+
+			if (inputKey == 'w')
+			{
+				if (myXPosition == 0) //올라갈 곳이 없으면 계속해서 while문을 돌린다.
+					continue;
+				else
+				{
+					map[myXPosition][myYPosition] = '#';
+					myXPosition--;
+					map[myXPosition][myYPosition] = 'O';
+				}
+			}
+
+			if (inputKey == 'a')
+			{
+				if (myYPosition == 0) //왼쪽으로 갈 곳이 없으면 계속해서 while문을 돌린다.
+					continue;
+				else
+				{
+					map[myXPosition][myYPosition] = '#';
+					myYPosition--;
+					map[myXPosition][myYPosition] = 'O';
+				}
+			}
+
+			if (inputKey == 's')
+			{
+				if (myXPosition == mapHeight - 1) //아래로 갈 곳이 없으면 계속해서 while문을 돌린다.
+					continue;
+				else
+				{
+					map[myXPosition][myYPosition] = '#';
+					myXPosition++;
+					map[myXPosition][myYPosition] = 'O';
+				}
+			}
+
+			if (inputKey == 'd')
+			{
+				if (myYPosition == mapWidth - 1) //오른쪽으로 갈 곳이 없으면 계속해서 while문을 돌린다.
+					continue;
+				else
+				{
+					map[myXPosition][myYPosition] = '#';
+					myYPosition++;
+					map[myXPosition][myYPosition] = 'O';
+				}
+			}
+		}
 	}
-
 #pragma endregion
-
-	cout << "< 난이도에 의해 HP와 몬스터를 조정합니다. >" << endl;
-	Sleep(1500);
-
-	cout << "영웅의 HP : " << heroesHp << endl;
-
-	Sleep(1000);
-	cout << "몬스터 숫자 : " << monsterCount << endl;
-
-	Sleep(1500);
-	cout << "모든 몬스터를 다 잡을 때까지 전투는 계속됩니다..." << endl;
-	cout << endl;
-	Sleep(2000);
-
-#pragma endregion
-
-	////맵에 내 위치, 탈출구, map을 저장하는 for문.
-	//for (int i = 0; i < mapHeight; i++)
-	//{
-	//	for (int j = 0; j < mapWidth; j++)
-	//	{
-	//		if (i == 0 && j == 0) //내 위치 생성
-	//		{
-	//			map[myXPosition][myYPosition] = 'O';
-	//		}
-	//		else if ((i == storeXPosition) && (j == storeYPosition)) //탈출구 출력
-	//		{
-	//			map[storeXPosition][storeXPosition] = '$';
-	//		}
-	//		else //map생성
-	//		{
-	//			map[i][j] = '#';
-	//		}
-	//	}
-	//}
 
 	//while (true)
 	//{
